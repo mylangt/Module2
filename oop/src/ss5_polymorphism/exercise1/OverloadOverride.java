774c489444dfc69ffb609622d5ed842307b31a53
mylangt <mylantt2001@gmail.com>
2024-08-11 13:17:00 +0700

    myla commit excercise oop

---
+++ oop/src/ss5_polymorphism/exercise1/OverloadOverride.java
@@ -0,0 +1,18 @@
+package ss5_polymorphism.exercise1;
+
+public class OverloadOverride {
+    public static void main(String[] args) {
+        /*
+        TH1: override
+        TH2: override
+        TH3: None
+        TH4: override
+        TH5: None
+        TH6: override
+        TH7: overload
+        TH8: None
+        TH9: overload
+        TH10: none
+         */
+    }
+}
