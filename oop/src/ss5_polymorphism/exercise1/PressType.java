396ba51ecd9cf14d4e5cbfea31b59ec58a2bfb58
mylangt <mylantt2001@gmail.com>
2024-08-11 13:23:42 +0700

    myla commit excercise oop

---
+++ oop/src/ss5_polymorphism/exercise1/PressType.java
@@ -0,0 +1,17 @@
+package ss5_polymorphism.exercise1;
+
+public class PressType {
+    public static void main(String[] args) {
+        //HocVien hocVien1 = new HocVien(); -> ko có casting
+        //HocVien hocVien2 = (SinhVien) hocVien1; -> downcasting, runtime
+        //ChuyenVien hocVien3 = (ChuyenVien) hocVien2; -> downcasting, runtime
+        //NVChinhThuc hocVien4 = hocVien3; -> upcasting
+        //HocVien hocVien5 = hocVien3; -> upcasting
+        //HocVien hocVien6 = (HocVien) hocVien2; -> upcasting, runtime
+        //HocVien hocVien7 = (NVQuanLy) hocVien6; -> downcasting, runtime
+        //NVChinhThuc hocVien8 = (NVChinhThuc) hocVien7; -> downcasting, runtime
+        //SinhVien hocVien9 = (SinhVien) hocVien3; -> downcasting, runtime
+        //HocVien hocVien10 = new SinhVien(); -> ko có casting
+        //ChuyenVien hocVien11 = (ChuyenVien) hocVien10; -> downcasting, runtime
+    }
+}
